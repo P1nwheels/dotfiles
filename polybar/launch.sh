1ec4439156186291123fb4cb51c7245c66caf742
P1nwheels <pnwheelz0x13@gmail.com>
2021-02-08 07:23:23 -0500

    Okay, now this is hot.

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -4,4 +4,4 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 polybar bspwm 2>&1 | tee -a /tmp/polybar1.log & disown
 polybar bspwm-2 2>&1 | tee -a /tmp/polybar1.log & disown
-
+polybar bspwm-3 2>&1 | tee -a /tmp/polybar1.log & disown
