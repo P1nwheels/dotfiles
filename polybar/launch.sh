761e498d3cbc43d899c2643441698bca1cb9cf56
P1nwheels <pnwheelz0x13@gmail.com>
2021-02-08 01:06:02 -0500

    TWO BARS BABY

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -3,3 +3,5 @@
 killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 polybar bspwm 2>&1 | tee -a /tmp/polybar1.log & disown
+polybar bspwm-2 2>&1 | tee -a /tmp/polybar1.log & disown
+
