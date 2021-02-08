bde4e9c39562be10ecb84d2982bab5f5ed065eb3
P1nwheels <pnwheelz0x13@gmail.com>
2021-02-08 00:34:02 -0500

    AHHHH

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -1,5 +1,5 @@
-#!/bin/bash
+#!/usr/bin/bash
 
 killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
-polybar bspwm 
+polybar bspwm 2>&1 | tee -a /tmp/polybar1.log & disown
